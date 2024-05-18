40b8cf3fe80c0c30d85645a03e2b49d59a057161
Amr Ali <amr.ali@damen.com.eg>
2024-05-18 23:46:41 +0300

    :hammer: Added middleware.js - Added isCashed middleware to return already processed requests

---
+++ internals/middlewares/middleware.js
@@ -0,0 +1,40 @@
+
+const cashing = require('../DBs/redis/index');
+
+
+const isCashed = async (request, h) => {
+
+    const { cashType =  } = request.app;
+
+    const { pathname, search} = request.url;
+
+    const { page: pageNumber = 1, pageSize = 20 } = request.query ?? {}
+
+    const start = (pageNumber - 1) * pageSize;
+
+    const end = start + pageSize - 1;
+
+    const movies_list = await cashing.getFromList(pathname + search, start, end);
+
+    try {
+
+        if (movies_list && Array.isArray(movies_list) && movies_list.length > 0 ) {
+
+            return h.response({ moviesCount : movies_list.length, movies: movies_list.map( movie => JSON.parse(movie)) }).takeover();
+
+        }
+
+        return h.continue;
+
+    } catch (e) {
+
+        return h.continue;
+
+    }
+
+}
+  
+
+module.exports = {
+    isCashed
+}
