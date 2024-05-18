0d80ba36e685558dc8132c765b092fbf1a456c2c
Amr Ali <amr.ali@damen.com.eg>
2024-05-18 23:58:11 +0300

    :hammer: Added index.js - the project main file

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+
+const { setupEndpoints } = require('./internals/transport/rest/endpoints')
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 8142,
+        host: '0.0.0.0'
+    });
+
+    await server.register(Inert);
+
+    setupEndpoints(server);
+
+    // Error handling
+    // server.ext('onPreResponse', (request, h) => {
+    //     const response = request.response;
+    //     if (response instanceof Error) {
+    //         console.error(response);
+    //         return Boom.internal('Internal server error');
+    //     }
+    //     return h.continue;
+    // });
+
+    await server.start();
+
+    console.log('Server running on %s', server.info.uri);
+};
+
+// process.on('unhandledRejection', (err) => {
+//     console.log(err);
+//     process.exit(1);
+// });
+
+init();
