9f944e127cb3e17cff6d81be8b6908d29468e227
Amr Ali <amr.ali@damen.com.eg>
2024-05-18 23:44:27 +0300

    :truck: Added endpoints.js file - include some of ready to consume endpoints

---
+++ internals/transport/rest/endpoints.js
@@ -0,0 +1,78 @@
+
+// handlers
+const MoviesHandlers = require('../../handlers/movies');
+const UserHandlers = require('../../handlers/users');
+
+// middlewares
+const { isCashed } = require('../../middlewares/middleware')
+
+const setupEndpoints = ( server ) => {
+
+    server.route({
+        method: 'GET',
+        path: '/helloworld',
+        options: {
+            // ext: {
+            //   onPreHandler: {
+            //     method: isCashed
+            //   }
+            // },
+            // pre: [{ method: isCashed }],
+            handler: (request, h) => {
+              return 'Hello, world!';
+            }
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/upload',
+        options: {
+            payload: {
+                output: 'stream',
+                parse: true,
+                allow: 'multipart/form-data',
+                multipart: true,
+                maxBytes: 1024 * 1024 * 5, // 5MB limit
+            }
+        },
+        handler: MoviesHandlers.uploadMoviesFromCSV        
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/v1/movies',
+        options: {
+            pre: [  
+                {
+                    method: (request, h) => {
+                        request.app.cashType = "list";
+                        return h.continue;
+                    },
+                    // assign: 'defaultCashType'
+                },
+                { 
+                    method: isCashed
+                }
+            ],
+            handler: MoviesHandlers.getMoviesHandler
+        }
+        
+        // handler: MoviesHandlers.getMoviesHandler
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/v1/movies/imdb/{imdb_id}',
+        handler: MoviesHandlers.getIMDBMovieByID
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/v1/user/profile/{user_id}',
+        handler: UserHandlers.getUserProfileInfo
+    });
+
+}
+
+module.exports = { setupEndpoints }
